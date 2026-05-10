8fcd103f7190b65dc594e2e4c4f295946e73521a
y.lepska <y.lepska@pin-up.tech>
2026-05-10 13:36:53 +0200

    Add useAuth composable with mock user

---
+++ frontend/src/app/layouts/Layout.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Menubar from 'primevue/menubar';
+import { useLayoutStore } from './layout.store';
+
+const layoutStore = useLayoutStore();
+
+const items = ref([
+  {
+    label: 'DASHBOARD',
+    icon: 'pi pi-home',
+    route: '/',
+  },
+  {
+    label: 'AI INTEGRATION',
+    icon: 'pi pi-spin pi-cog',
+    route: '/integration',
+  },
+  {
+    label: 'DOCS',
+    icon: 'pi pi-file',
+    route: '/docs',
+  },
+]);
+</script>
+
+<template>
+  <header>
+    <Menubar :model="items">
+      <template #item="{ item, props }">
+        <router-link custom :to="item.route" v-slot="{ href, navigate }">
+          <a :href="href" v-bind="props.action" @click="navigate">
+            <span :class="item.icon"></span>
+            <span>{{ item.label }}</span>
+          </a>
+        </router-link>
+      </template>
+    </Menubar>
+  </header>
+
+  <main class="dv-main">
+    <RouterView />
+  </main>
+</template>
+
